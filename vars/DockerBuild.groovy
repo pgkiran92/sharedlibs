a429e01693445806b998ebb8fd8b6141456a25bc
pgkiran92 <79925402+pgkiran92@users.noreply.github.com>
2022-09-08 17:19:51 +0530

    Create DockerBuild.groovy

---
+++ vars/DockerBuild.groovy
@@ -0,0 +1,11 @@
+def call(String project, String hubUser) {
+    sh "docker image build -t ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER} ."
+    withCredentials([usernamePassword(
+            credentialsId: "dockerhub-cred",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+        sh "docker login -u '$USER' -p '$PASS'"
+    }
+    sh "docker image push ${hubUser}/${project}:beta-${env.BRANCH_NAME}-${env.BUILD_NUMBER}"
+}
